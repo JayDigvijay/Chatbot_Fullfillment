fd9a2e2b0b7f4341e3e3183b123550af0bcf2185
JayDigvijay <40368280+JayDigvijay@users.noreply.github.com>
2019-08-03 21:45:10 +0530

    Update index.js

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -16,39 +16,9 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
- 
-  function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
-  }
- 
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-  }
-  function dayschedule(agent) {
- 	const weekday = agent.parameters.weekday; 
-    //agent.add(weekday);
-    var start = 1;
-    var i;
-	if(weekday == 7)
-  		agent.add(`No classes on your schedule. Enjoy!`);
-	else
-		agent.add(`Here is your schedule for the day-`);  
-	return admin.database().ref('Class').once("value").then((snapshot) => {
-  for(i = start; i <11; i++){
-    
-    		var key = weekday + "," + i;
-    		var time = i+7;
-    		var classtime = snapshot.child(key).val();
-   		  agent.add(`At `+ time + `:00 hours, you have a ` + classtime);
-  }
-});
- }
-                                                      
-function rs(agent){
- var d = new Date();
+    var d = new Date();
     var day = d.getDay();
-   	var hour = d.getHours(), min = d.getMinutes();
+    var hour = d.getHours(), min = d.getMinutes();
     if((min + 30) >= 60){
       hour ++;
     }
@@ -59,10 +29,38 @@ function rs(agent){
       day++;
     }
     hour %= 24;
-  const t = 9; //Fill required time of the day in hours for testing.
-  const dy = 3;// Fill required day of the week for testing. 0 = Sunday, 6 = Saturday 
-  //day = dy;  // Uncomment for testing.
-  //hour = t;
+  
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+  function dayschedule(agent) {
+  var weekday = agent.parameters.weekday; 
+    //agent.add(weekday);
+    var start = 1;
+    var i;
+  if(weekday == 7)
+      agent.add(`No classes on your schedule. Enjoy!`);
+  else{
+    agent.add(`Here is your schedule for the day-`);  
+  return admin.database().ref('Class').once("value").then((snapshot) => {
+  for(i = start; i <11; i++){
+    
+        var key = weekday + "," + i;
+        var time = i+7;
+        var classtime = snapshot.child(key).val();
+        agent.add(`At `+ time + `:00 hours, you have a ` + classtime);
+  }
+});
+    }
+ }
+                                                      
+function rs(agent){
+ 
   var start = hour-6;
   var i;
 if(hour > 17 || day == 7)
@@ -90,6 +88,6 @@ return admin.database().ref('Class').once("value").then((snapshot) => {
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
   intentMap.set('dayschedule', dayschedule);
-  intentMap.set('rs', rs);
+  intentMap.set('todayschedule', rs);
   agent.handleRequest(intentMap);
 });
