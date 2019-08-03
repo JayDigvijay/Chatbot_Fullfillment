5ce7c09bdc0199615931f6f50484d2a487059653
JayDigvijay <40368280+JayDigvijay@users.noreply.github.com>
2019-08-03 17:00:42 +0530

    Timetable Reader Added

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,13 +1,17 @@
 // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
 // for Dialogflow fulfillment library docs, samples, and to report issues
-'use strict';
- 
+
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
- 
+const admin = require('firebase-admin');
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'ws://calculator-a001e.firebaseio.com/',
+});
+
+
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
- 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
@@ -39,12 +43,49 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     agent.add(`The time is ` + hour + `:` + min + ` and the day of the week is ` + day);
     
   }
+function rs(agent){
+ var d = new Date();
+    var day = d.getDay();
+   	var hour = d.getHours(), min = d.getMinutes();
+    if((min + 30) >= 60){
+      hour ++;
+    }
+    min += 30;
+    min %= 60;
+    hour += 5;
+    if(hour >= 24){
+      day++;
+    }
+    hour %= 24;
+  const t = 9; //Fill required time of the day in hours for testing.
+  const dy = 3;// Fill required day of the week for testing. 0 = Sunday, 6 = Saturday 
+  //day = dy;  // Uncomment for testing.
+  //hour = t;
+  var start = hour-6;
+  var i;
+  
+agent.add(`Here is your schedule for the rest of the day-`);  
+return admin.database().ref('Class').once("value").then((snapshot) => {
+  for(i = start; i <11; i++){
+    
+    var key = day + "," + i;
+    var time = i+7;
+    var classtime = snapshot.child(key).val();
+    agent.add(`At `+ time + `:00 hours, you have a ` + classtime);
+   
+  }
+      
+    });
+
+}
+
+
 
  
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
   intentMap.set('tnd', tnd);
-  // intentMap.set('your intent name here', googleAssistantHandler);
+  intentMap.set('rs', rs);
   agent.handleRequest(intentMap);
 });
